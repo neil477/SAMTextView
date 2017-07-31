2fd49a42bdb26f4b8ea938c1d66608eb7e23b1fc
Neil B <neil477@gmail.com>
2017-07-31 11:23:04 -0700

    SAMText Framework

---
+++ SAMTextView/SAMText/SAMText/SAMText.h
@@ -0,0 +1,19 @@
+//
+//  SAMText.h
+//  SAMText
+//
+//  Created by Neil Bhakta on 7/31/17.
+//  Copyright © 2017 pushd, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SAMText.
+FOUNDATION_EXPORT double SAMTextVersionNumber;
+
+//! Project version string for SAMText.
+FOUNDATION_EXPORT const unsigned char SAMTextVersionString[];
+
+#import <SAMText/SAMTextView.h>
+
+
